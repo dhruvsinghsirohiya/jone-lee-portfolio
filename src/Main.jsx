0eca40802186ff901cc54bff979760419396bf4e
Dhruv Singh Sirohiya <dhruvsinghsirohiyadss@gmail.com>
2024-10-02 20:12:21 +0530

    added

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './comman/navbar/Navbar';
+import HomeSection from './screen/HomeSection';
 
 const Main = () => {
     return (
         <div className='w-[100%]'>
             <Navbar/>
+            <HomeSection/>
         </div>
     );
 }
