8c1f2f602e8740eca1427595ce4352585db4e527
dhruvsinghsirohiya <dhruvsinghsirohiyadss@gmail.com>
2024-10-05 19:31:08 +0530

    adde

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,14 +2,23 @@ import React from "react";
 import Navbar from "./comman/navbar/Navbar";
 import HomeSection from "./homePage/HomeSection";
 import FeatureSection from "./homePage/FeatureSection";
+import { IoMdArrowRoundUp } from "react-icons/io";
 
 const Main = () => {
   return (
-    <div className="w-[100%]">
-      <Navbar />
-      <HomeSection />
-      <FeatureSection/>
-    </div>
+    <>
+      <div id="#home" className="w-[100%] relative">
+        <Navbar />
+        <HomeSection />
+        <FeatureSection />
+        <a
+          href="#home"
+          className="bg-[#ff014f] fixed text-[30px] rounded-full hover:text-[#ff014f] p-3 cursor-pointer hover:bg-white right-[1.5%] bottom-[1.5%]"
+        >
+          <IoMdArrowRoundUp />
+        </a>
+      </div>
+    </>
   );
 };
 
--- src/homePage/FeatureSection.jsx
+++ src/homePage/FeatureSection.jsx
@@ -57,7 +57,7 @@ const FeatureSection = () => {
   };
 
   return (
-    <div className="w-[90%] py-20 mx-auto">
+    <div id="features" className="w-[90%] py-20 mx-auto">
       <h6 className="text-[#ff014f] text-[14px] uppercase tracking-[2px]">
         Features
       </h6>
